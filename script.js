d7370202acc482f6a805eef6b9f1ef589192d8e9
Anantaeka12 <55143079+musky12@users.noreply.github.com>
2024-06-19 23:05:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+function cekKhodam() {
+  var nama = document.getElementById("nama").value;
+  var loading = document.getElementById("loading");
+  var hasil = document.getElementById("hasil");
+  var namaHasil = document.getElementById("nama-hasil");
+  var khodamElem = document.getElementById("khodam");
+  var periksa = document.getElementById("periksa");
+  var ulang = document.getElementById("ulang");
+  var inputContainer = document.getElementById("input-container");
+  var introText = document.getElementById("intro-text");
+
+  if (nama === "") {
+    alert("Silakan masukkan nama.");
+    return;
+  }
+
+  loading.classList.remove("hidden");
+  hasil.classList.add("hidden");
+  periksa.classList.add("hidden");
+  inputContainer.classList.add("hidden");
+  introText.classList.add("hidden");
+
+  setTimeout(function () {
+    loading.classList.add("hidden");
+    var khodam = getKhodam(nama);
+    namaHasil.innerText = nama.toUpperCase();
+    khodamElem.innerText = khodam;
+    hasil.classList.remove("hidden");
+    ulang.classList.remove("hidden");
+  }, 2000);
+}
+
+function getKhodam(nama) {
+  var khodamList = [
+    "Harimau",
+    "Naga Mahjong",
+    "Burung",
+    "Serigala",
+    "Kucing",
+    "Gagang Pintu",
+    "Ular Kontol",
+    "Kecoa",
+    "Babi",
+    "kipas cosmos",
+    "PELO",
+    "Gak Onok Khodam e Matio ae",
+  ];
+  var index = nama.length % khodamList.length;
+  return khodamList[index];
+}
+
+function ulangCek() {
+  var nama = document.getElementById("nama");
+  var hasil = document.getElementById("hasil");
+  var periksa = document.getElementById("periksa");
+  var ulang = document.getElementById("ulang");
+  var inputContainer = document.getElementById("input-container");
+  var introText = document.getElementById("intro-text");
+
+  nama.value = "";
+  hasil.classList.add("hidden");
+  periksa.classList.remove("hidden");
+  ulang.classList.add("hidden");
+  inputContainer.classList.remove("hidden");
+  introText.classList.remove("hidden");
+}
